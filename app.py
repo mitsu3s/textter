7afa9c10a81e605505911f8d16c8c7e630e72ad2
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-01-18 15:59:01 +0900

    Updating database tables

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def hash_password(password:str)->str:
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    userimage = db.Column(db.LargeBinary)
     password = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
